d7c5a5b6bf4473233b8e8b50ba877773d70d8fd3
AlxG416 <Alex153269@yandex.ru>
2026-03-23 10:26:59 +0300

    Update MessagesPage.tsx

--- client/src/pages/MessagesPage/MessagesPage.tsx
+++ client/src/pages/MessagesPage/MessagesPage.tsx
@@ -5,9 +5,9 @@ import Message from "../../components/Message/Message";
 import "./MessagesPage.css";
 
 var data = [
-    {ilocName: 'Анатолий', ilocSurname: 'Рубликов', message: 'Верни мне мой пяточёк!', id: 1},
-    {ilocName: 'Красный', ilocSurname: 'Баклажанчик', message: 'Приготовь меня', id: 2},
-    {ilocName: 'Анастасия', ilocSurname: 'Шишкина', message: 'Как дела? Приходи ко мне, тут весело)))', id: 3}
+    {ilocName: 'Анатолий', ilocSurname: 'Лебедев', message: 'Привет, как дела?', id: 1},
+    {ilocName: 'Андрей', ilocSurname: 'Смирнов', message: 'Ты уже сходил в магазин?', id: 2},
+    {ilocName: 'Евгений', ilocSurname: 'Белов', message: 'Я сегодня занят, встретиться не получится.', id: 3}
 ];
 
 var MessagesPage: React.FC = () => {
